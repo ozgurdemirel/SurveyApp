31a91cd41138986b6d8b80dcb5718addecb57991
ozgur <demirelozgur@gmail.com>
2017-04-02 00:28:39 +0300

    angular ui

--- src/main/webapp/survey/sections/survey/survey.ctrl.js
+++ src/main/webapp/survey/sections/survey/survey.ctrl.js
@@ -2,6 +2,22 @@ angular
     .module('app.core')
     .controller('surveyController',function () {
             var vm = this;
+            vm.questionCount = 1;
+            vm.question='';
+            vm.answers = [''];
+
+            vm.addQuestion = function () {
+                console.log('dasdasd');
+                vm.answers.push('');
+            };
+
+            vm.save = function () {
+                console.log(vm.answers);
+            };
+
+            vm.remove = function (i) {
+                vm.answers.splice(i,1);
+            };
 
         }
     );
